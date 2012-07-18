573d433da000a1756edc9e40ecdf1e8a9b2e94a3
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-07-18 10:48:59 +0530

    morning update

--- excer1/js/sandbox.js
+++ excer1/js/sandbox.js
@@ -5,16 +5,16 @@ console.log("all of the div elements that have a class of module:");
 console.log(jq("div.module"));
 //2.
 console.log("\n3 ways to select");
-console.log(jq("#myListItem"));
+console.log(jq("#myList li:eq(2)"));
 console.log(jq("ul#myList li:nth-child(3)"));
-console.log(jq("li#myListItem"));//this is best
+console.log(jq(jq("ul#myList").children()[2]));//this is best
 
 //3.
 console.log("\nlabel for the search input using an attribute selector");
 console.log(jq("label[for='q']"));
 
 //4.
-console.log("\nNo. of hidden elements on the page:"+jq('*:visible').length);
+console.log("\nNo. of hidden elements on the page:"+jq('*:hidden').length);
 //5.
 console.log("\nNo. of image elements on the page have an alt attribute:"+jq('img[alt]').length);
 
